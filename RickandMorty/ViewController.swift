fd1a3f6e3be4fb61f5f762ec57b2678363e0c4ae
wamuni <boshi.pan@outlook.com>
2024-02-20 21:13:03 +0000

    project set up

---
+++ RickandMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMorty
+//
+//  Created by 潘博石 on 20/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
